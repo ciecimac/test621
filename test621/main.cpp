e8e9f94c24731a7d6a6b376e5384f148d9b137d2
admin <admin@bogon>
2018-06-21 09:18:29 +0800

    Initial Commit

---
+++ test621/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test621
+//
+//  Created by admin on 18/6/21.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
