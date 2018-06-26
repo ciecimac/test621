f564d6b532ef7e581616f79456ff966d79041dd5
ciecimac <ciecclm@imnu.edu.cn>
2018-06-26 15:37:45 +0800

    改变表格标题
    将中文改成英文

--- test621/main.cpp
+++ test621/main.cpp
@@ -9,9 +9,14 @@
 using namespace std;
 int main()
 {
-    FILE *fp;
-    freopen("/Users/admin/Desktop/data/out.txt","w",fp);
+    //FILE *fp=stdin;
+    //string s;
+   // char s[2000]="";
+   // freopen("/Users/admin/Desktop/test621/test621/studentdata.csv","r",stdin);
+    freopen("/Users/admin/Desktop/test621/test621/studentdataout.csv","w",stdout);
     //cout<<"hello"<<endl;
-    fprintf(fp,"haha");
+   // cin>>s;
+    cout<<"name,sex,birthday\n"<<endl;
+    //fprintf(fp,"haha");
     return 0;
 }
