93bdd74cee6f2352b4d1edf097ae3204f7ce75ce
ciecimac <ciecclm@imnu.edu.cn>
2018-06-26 15:03:32 +0800

    增加数据文件
    在系统中增加了名为studentdata.csv的数据文件，此文件作为数据输入源

--- test621/main.cpp
+++ test621/main.cpp
@@ -4,13 +4,14 @@
 //
 //  Created by admin on 18/6/21.
 //  Copyright © 2018年 admin. All rights reserved.
-//
 
 #include <iostream>
 using namespace std;
-int main() {
-    int i;
-    for(i=0;i<100;i++)
-        cout<<i<<endl;
+int main()
+{
+    FILE *fp;
+    freopen("/Users/admin/Desktop/data/out.txt","w",fp);
+    //cout<<"hello"<<endl;
+    fprintf(fp,"haha");
     return 0;
 }
